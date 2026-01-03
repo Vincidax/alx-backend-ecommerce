4f397030e83883cca23bd54aef9449e37bc9bc68
vincidax <vincentdx18@gmail.com>
2026-01-03 18:53:45 +0200

    adding views

---
+++ EcommerceAPI/DjangoAPIapp/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+from .models import Category, Book, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title'
+        )
+        
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'category',
+            'isbn',
+            'pages',
+            'price',
+            'stock',
+            'description',
+            'imageUrl',
+            'status',
+            'date_created'
+        )
+        model = Book
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'product_tag',
+            'name',
+            'category',
+            'price',
+            'imageUrl',
+            'status',
+            'date_created'
+        )
+        model = Product
--- EcommerceAPI/DjangoAPIapp/urls.py
+++ EcommerceAPI/DjangoAPIapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-
+from .views import ListCategory, DetailCategory, ListBook, DetailBook, ListProduct, DetailProduct
 urlpatterns = [
-    
+    path('categories', ListCategory.as_view(), name='categorie'),
+    path('categories/<int:pk>/', DetailCategory.as_view(), name= 'singlecategory'),
+    path('book', ListBook.as_view(), name='books'),
+    path('book/<int:pk>/', DetailBook.as_view(), name= 'singlebook'),
+    path('product', ListProduct.as_view(), name='categorie'),
+    path('product/<int:pk>/', DetailProduct.as_view(), name= 'singleproduct'),
 ]
